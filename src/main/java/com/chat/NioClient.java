ab2d2dd14461c7cf6589d9368f85efd48dde28b2
christma <liuchuanmumumu@gmail.com>
2020-10-07 22:33:31 +0800

    Add method

--- src/main/java/com/chat/NioClient.java
+++ src/main/java/com/chat/NioClient.java
@@ -1,8 +1,34 @@
 package com.chat;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
 /**
  * NIO 客户端
  */
 
 public class NioClient {
+
+    // TODO: 2020/10/7  启动
+
+    public void start() throws IOException {
+        // 连接服务端
+        SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress("127.0.0.1", 8000));
+        // 向服务器端发送数据
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String request = scanner.nextLine();
+            if (request != null && request.length() > 0) {
+                socketChannel.write(Charset.forName("UTF-8").encode(request));
+            }
+        }
+        // 接受服务器端的相应
+    }
+
+    public static void main(String[] args) throws IOException {
+        new NioClient().start();
+    }
 }
